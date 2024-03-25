abb1b344fad01e56c8729eb32b5546a2bb58e9f3
Bruno Amorim <bruno.tolentino@basis.com.br>
2024-03-25 10:53:15 -0300

    feat: removendo logica não utilizadas

--- ms-cobranca/app/Services/ProcessDebtService.php
+++ ms-cobranca/app/Services/ProcessDebtService.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Services\DebtService;
 use App\Imports\ChargesImport;
 use App\Services\TicketService;
+use App\Services\InvoiceService;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\HeadingRowImport;
@@ -27,19 +28,13 @@ class ProcessDebtService implements IProcessDebtInterface
     public $statusCode;
     public $msg;
     public $errorCode;
-    private TicketService $ticketService;
-    private DebtService $debtService;
 
     public function __construct(
-        TicketService $ticketService,
-        DebtService $debtService
+        private readonly TicketService $ticketService,
+        private readonly DebtService $debtService,
+        private readonly InvoiceService $invoiceService,
     )
     {
-        $this->ticketService = $ticketService;
-        $this->debtService = $debtService;
-
-        $this->initializeServiceProperties();
-
        /** Maintain the formatting of the CSV title. */
         HeadingRowFormatter::default('none');
     }
@@ -72,30 +67,31 @@ class ProcessDebtService implements IProcessDebtInterface
         return false;
     }
 
-    public function processCharge(ChargeDTO $chargeDTO)
+    private function getInvoicesByCharge(ChargeDTO $chargeDTO): array
+    {
+        $response = $this->invoiceService->generateInvoice($chargeDTO);
+
+        if(!isset($response['status']))
+        {
+            $this->storeLogData($response, "error_request_integrate_invoice");
+            throw new Exception('Error to get Invoice.');
+        }
+
+        return $response;
+    }
+
+    public function processCharge(ChargeDTO $chargeDTO): bool
     {
         try {
-                $response = $this->performRequest('POST', 'transactions', $chargeDTO->toArray());
+            $invoice = $this->getInvoicesByCharge($chargeDTO);
 
-                if(!isset($response['status']))
-                {
-                    $this->storeLogData($response, "error_request_integrate_ticket");
-                }
+            $ticket = $this->ticketService->storeTicket(array_merge($invoice['data'], $chargeDTO->toArray()));
 
-                $ticket = $this->ticketService->storeTicket(array_merge($response['data'], $chargeDTO->toArray()));
+            $this->debtService->updateDebt($ticket->ticketId, $chargeDTO->debtId);
 
-                if(! $this->debtService->updateDebt($ticket->ticketId, $chargeDTO->debtId))
-                {
-                    throw new Exception("Error to Update debt", 400);
+            $this->ticketService->publishTicketMail($chargeDTO);
 
-                }
-
-                if(!$this->publishTicketMail($chargeDTO))
-                {
-                    return false;
-                }
-
-                return true;
+            return true;
 
         } catch (Throwable $th) {
             $this->statusCode = StatusServiceEnum::STATUS_CODE_ERRO;
@@ -175,111 +171,5 @@ class ProcessDebtService implements IProcessDebtInterface
         } finally {
             fclose($file);
         }
-    }
-
-    public function processListDebt(Request $request): bool
-    {
-        try {
-
-            $title = $this->getTitle($request);
-
-            $this->validateTitles($title);
-
-            $charges = Excel::toArray(new ChargesImport, $request->file('listDebt'));
-
-            return $charges;
-
-        } catch (ValidationException $e) {
-            $failures = $e->failures();
-
-            $errors = [];
-
-            foreach ($failures as $failure) {
-                $failure->row();
-                $failure->attribute();
-                array_push($errors, $failure->errors());
-                $failure->values();
-
-            }
-
-            $this->statusCode = StatusServiceEnum::STATUS_CODE_ERRO;
-            $this->msg = json_encode($errors);
-            $this->errorCode = 400;
-
-            return false;
-
-        } catch (Throwable $th) {
-            $this->statusCode = StatusServiceEnum::STATUS_CODE_ERRO;
-            $this->msg = $th->getMessage();
-            $this->errorCode = 400;
-
-            return false;
-        }
-    }
-
-    public function validateTitles(array $title): bool
-    {
-        $defaultTitile = [
-            'name',
-            'governmentId',
-            'email',
-            'debtAmount',
-            'debtDueDate',
-            'debtId'
-        ];
-
-        $diff = array_diff($defaultTitile, $title);
-
-        if (empty($title))
-        {
-            throw new Exception("Uploaded file is empty!", 400);
-        }
-        else if(empty($diff)){
-
-            return true;
-        }
-        else
-        {
-            throw new Exception("The data sent does not match what was expected!", 400);
-        }
-
-        return false;
-    }
-
-    /**
-     * pega os dados do header do arquivo
-     */
-    public function getTitle(Request $request): array
-    {
-        try {
-
-            $title = $headings = (new HeadingRowImport)->toArray($request->file('listDebt'));
-
-            return $title[0][0];
-
-        } catch (Throwable $th) {
-
-            $this->statusCode = StatusServiceEnum::STATUS_CODE_ERRO;
-            $this->msg = "Invalid Spreadsheet file";
-            $this->errorCode = 400;
-
-            return [];
-        }
-
-    }
-
-    public function publishTicketMail(ChargeDTO $attributesDTO): bool
-    {
-        try {
-            notificationChargeCustomer::dispatch($attributesDTO->toArray());
-
-        } catch (Throwable $th) {
-            $this->storeLogData(['message' => $th->getMessage()], "error_request_integrate_ticket");
-
-            return false;
-        }
-
-        return true;
-
     }
 }
